86f8b141abd346144ef09ca8841ecad2964f1bce
Jeremiah Flaga <flaga.jeremiah@gmail.com>
2024-04-07 23:37:39 +0800

    fix: minor technical mistake on page 68 - expectedVisits

--- listings/05-07.cs
+++ listings/05-07.cs
@@ -6,6 +6,6 @@ public class LogVisit
     {
         _db.Execute(@“UPDATE Users SET visits=visits+1
                       WHERE user_id=@p1 and visits = @p2”,
-                      userId, visits);
+                      userId, expectedVisits);
     }
 }
